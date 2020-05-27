173530a4096e2327646ad78efc5c220a6c5d1903
Nora P <veganmuffcakes@gmail.com>
2020-05-26 23:04:43 -0700

    current working version, dirty draft

--- StudentGrades.java
+++ StudentGrades.java
@@ -1,17 +1,29 @@
 /**
- * 
+ * StudentGrades.java
  */
-
+/********************************************************
+ * REQUIREMENTS - ITC155 MIDTERM 
+ * Problem 2: 
+ * Given a HashMap pre-filled with student names as keys and grades
+ * as values, complete main() by reading in the name of a student, 
+ * outputting their original grade, and then reading in and outputting 
+ * their new grade.
+ * 
+ * Ex: If the input is:
+ * Quincy Wraight
+ * 73.1
+ * 
+ * the output is:
+ * Quincy Wraight's original grade: 65.4
+ * Quincy Wraight's new grade: 73.1
+ *********************************************************/
 
 /**
- * @author npeachey
- * TODO add requirements - finish WHAT YOU HAVE FIRST THEN LOOK
- * TODO iniitalize repo in project folder to track
- * TODO copy/paste code to output EXACTLY
- * TODO tests
- * TODO add comments
+ * @author npeachey TODO add requirements - finish WHAT YOU HAVE FIRST THEN LOOK
+ *         TODO iniitalize repo in project folder to track TODO copy/paste code
+ *         to output EXACTLY TODO tests TODO add comments
  * 
- * TODO if time (add explanation of why it was challenging - Q's for QnA
+ *         TODO if time (add explanation of why it was challenging - Q's for QnA
  *
  */
 public class StudentGrades {
