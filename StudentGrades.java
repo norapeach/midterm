d3e2c445764e51886c6d2e98bf112d45269add49
Nora P <veganmuffcakes@gmail.com>
2020-05-26 23:13:04 -0700

    add content from previous version to clean

--- StudentGrades.java
+++ StudentGrades.java
@@ -18,22 +18,84 @@
  * Quincy Wraight's new grade: 73.1
  *********************************************************/
 
+import java.util.Scanner;
+import java.util.HashMap;
+
 /**
- * @author npeachey TODO add requirements - finish WHAT YOU HAVE FIRST THEN LOOK
- *         TODO iniitalize repo in project folder to track TODO copy/paste code
- *         to output EXACTLY TODO tests TODO add comments
- * 
- *         TODO if time (add explanation of why it was challenging - Q's for QnA
- *
+ * @author Nora P.
+ * @version 05/19/20 Description: This program prompts the user to enter a
+ *          student name, If that student is present in the existing record
+ *          (HashMap studentGrades), the existing grade is displayed on the
+ *          console and the user is prompted to enter a new grade. That input
+ *          replaces the value mapped to the key (studentName) and the updated
+ *          grade record is displayed on the console. TODO: 5/22
  */
 public class StudentGrades {
 
+	public static void main(String[] args) throws Exception {
+		// Problem 2 Starter Code: 
+		Scanner scnr = new Scanner(System.in);
+		String studentName = "";
+		double studentGrade = 0;
+
+		HashMap<String, Double> studentGrades = new HashMap<String, Double>();
+
+		// Students's grades (pre-entered)
+		studentGrades.put("Harry Rawlins", 84.3); // Harry Rawlin 90.0
+		studentGrades.put("Stephanie Kong", 91.0);
+		studentGrades.put("Shailen Tennyson", 78.6);
+		studentGrades.put("Quincy Wraight", 65.4);
+		studentGrades.put("Janine Antinori", 98.2);
+
+		///  START  ///
+		prompt("Enter the full student name (ex: Quincy Wraight 73.1) >>> ");
+
+		try { // attempt to catch exceptions 
+			// TODO 5/24 change to nextLine --> reference entire token in variable
+			if (scnr.hasNext()) { // next line, next double - no need for adding space - 
+				studentName = scnr.next();
+				studentName += " " + scnr.next();
+
+				// pre: student is present as key in studentGrades
+				if (studentGrades.containsKey(studentName)) {
+					// get current value associated to key and output
+					double original = studentGrades.get(studentName);
+					System.out.println(studentName + "'s original grade: " + original + "\n"); // output the original
+																								// grade
+					// TODO 5/24 remove to only prompt once then collect line and then next double
+					// Collect input to update key/value pair
+					prompt("Enter " + studentName + "'s new grade >>> ");
+						studentGrade = scnr.nextDouble(); // consume token and store as studentGrade
+						scnr.close(); // Close scanner
+
+						// Replace original with new value and output to console
+						studentGrades.replace(studentName, original, studentGrade);
+						System.out.println(studentName + "'s new grade: " + studentGrades.get(studentName) + "\n");
+					} // END post: records updated and displayed
+
+				} else { // key not present
+					System.out.println("No records found for " + studentName);
+
+				} // END if - key not present
+
+			} // END outer if
+
+//			
+
+		} catch (Exception e) {
+			System.out.println("Exception thrown: " + e);
+		} // END try/catch block
+
+	} // END main
+
 	/**
-	 * @param args
+	 * Prompt outputs the String content to the console - to be used in combination
+	 * with Scanner(System.in)
+	 * 
+	 * @param content The string to be included as the output text for user input
+	 *                prompt
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void prompt(String content) {
+		System.out.print(content);
 	}
-
-}
+} // END class
